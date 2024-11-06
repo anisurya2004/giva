4675e953638bd07146997a1ef35f7ea7f0cd0af1
Anisurya <anisuryaj22@iitk.ac.in>
2024-11-06 20:13:23 +0530

    Further changes in the add-product page

--- frontend/pages/add-product.tsx
+++ frontend/pages/add-product.tsx
@@ -2,11 +2,9 @@ import ProductForm from '../components/form';
 import { useRouter } from 'next/router';
 
 const AddProductPage = () => {
-  const router = useRouter();
-  const { id, name: initialName, description: initialDescription, price: initialPrice, quantity: initialQuantity } = router.query;
   return (
     <div className="container">
-      <h1 className="text-center my-4">{id ? 'Edit Product' : 'Add New Product'}</h1>
+      <h1 className="text-center my-4">Product Form</h1>
       <ProductForm />
     </div>
   );
