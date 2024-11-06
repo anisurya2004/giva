2fa8b4c827d525c72cbc0b6313e712fff100822d
Anisurya <anisuryaj22@iitk.ac.in>
2024-11-06 20:09:43 +0530

    Form page has been edited

--- frontend/pages/add-product.tsx
+++ frontend/pages/add-product.tsx
@@ -1,9 +1,12 @@
 import ProductForm from '../components/form';
+import { useRouter } from 'next/router';
 
 const AddProductPage = () => {
+  const router = useRouter();
+  const { id, name: initialName, description: initialDescription, price: initialPrice, quantity: initialQuantity } = router.query;
   return (
     <div className="container">
-      <h1 className="text-center my-4">Add a New Product</h1>
+      <h1 className="text-center my-4">{id ? 'Edit Product' : 'Add New Product'}</h1>
       <ProductForm />
     </div>
   );
